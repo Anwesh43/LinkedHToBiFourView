cd330e795fb164c32785ec6ba199e35ffdeab1b3
Anwesh43 <anweshthecool0@gmail.com>
2019-04-04 08:02:46 +0530

    adding HToBiFourView class

--- htobifourview/src/main/java/com/anwesh/uiprojects/htobifourview/HToBiFourView.kt
+++ htobifourview/src/main/java/com/anwesh/uiprojects/htobifourview/HToBiFourView.kt
@@ -55,3 +55,21 @@ fun Canvas.drawHTBFNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class HToBiFourView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
