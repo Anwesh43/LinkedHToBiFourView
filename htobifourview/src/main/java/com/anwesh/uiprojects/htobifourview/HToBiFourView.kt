7097311c92bcb777003e5af9dbff5ccf4ac79b2f
Anwesh43 <anweshthecool0@gmail.com>
2019-04-04 08:14:45 +0530

    adding HTBFNode class

--- htobifourview/src/main/java/com/anwesh/uiprojects/htobifourview/HToBiFourView.kt
+++ htobifourview/src/main/java/com/anwesh/uiprojects/htobifourview/HToBiFourView.kt
@@ -121,6 +121,49 @@ class HToBiFourView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class HTBFNode(var i : Int, val state : State = State()) {
+        private var next : HTBFNode? = null
+        private var prev : HTBFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = HTBFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHTBFNode(i,state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HTBFNode {
+            var curr : HTBFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
     companion object {
         fun create(activity : Activity) : HToBiFourView {
             val view : HToBiFourView = HToBiFourView(activity)
