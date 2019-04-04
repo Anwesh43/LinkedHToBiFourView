0499783c0136e43ec6e41bf8b267a7b9418043f4
Anwesh43 <anweshthecool0@gmail.com>
2019-04-04 07:48:25 +0530

    adding libraries and constants

--- htobifourview/src/main/java/com/anwesh/uiprojects/htobifourview/HToBiFourView.kt
+++ htobifourview/src/main/java/com/anwesh/uiprojects/htobifourview/HToBiFourView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.htobifourview
  * Created by anweshmishra on 04/04/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
